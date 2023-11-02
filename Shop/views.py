68c6c95f70c9f7278afcc04f73858e3334501471
Joy-Ndirangu <joyndirangu18@gmail.com>
2023-11-02 13:56:20 +0300

    changed links to be active

--- Shop/views.py
+++ Shop/views.py
@@ -4,28 +4,28 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, "index.html")
+    return render(request, "index.html",{ 'navbar': 'home'})
 
 
 def shop(request):
-    return render(request, "shop.html")
+    return render(request, "shop.html",{ 'navbar': 'shop'})
 
 
 def about(request):
-    return render(request, "about.html")
+    return render(request, "about.html", { 'navbar': 'about'})
 
 
 def services(request):
-    return render(request, "services.html")
+    return render(request, "services.html", { 'navbar': 'services'})
 
 def blog(request):
-    return render(request,"blog.html")
+    return render(request,"blog.html", { 'navbar': 'blog'})
 
 def contact(request):
-    return render(request,"contact.html")
+    return render(request,"contact.html", { 'navbar': 'contact'})
 
 def cart(request):
-    return  render(request, "cart.html")
+    return  render(request, "cart.html", { 'navbar': 'cart'})
 
 def checkout(request):
-    return render(request, "checkout.html")
+    return render(request, "checkout.html",{ 'navbar': 'checkout'})
