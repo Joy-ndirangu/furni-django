dc1c35075fc3d9f05edc48f358d201e3eb6a3d87
Joy-Ndirangu <joyndirangu18@gmail.com>
2023-11-02 09:39:54 +0300

    first commit

---
+++ Shop/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+
+app_name = "Shop"
+
+urlpatterns = [
+
+    path('', views.home, name="home"),
+    path('shop/', views.shop, name="shop"),
+    path('about/', views.about, name="about"),
+    path('services/', views.services, name="services"),
+    path('blog/', views.blog, name="blog"),
+    path('contact/', views.contact, name="contact"),
+    path('cart/',views.cart, name="cart"),
+    path('checkout/', views.cart, name="checkout")
+
+]
